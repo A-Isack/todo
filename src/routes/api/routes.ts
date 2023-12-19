8e6848534f44961c0a63798c7164bd3e345d1e96
A-Isack <61166785+A-Isack@users.noreply.github.com>
2023-12-19 19:15:00 +0200

    seeds route added

--- src/routes/api/routes.ts
+++ src/routes/api/routes.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import {User}  from "../../models/users";
 import {List}  from "../../models/list";
+import { faker } from "@faker-js/faker";
 
 const router  : express.Router = express.Router()
 
@@ -120,11 +121,59 @@ router.get(`/todo/`, async(req : Request, res: Response)=>{
                 res.send("No records found")
             }
             })
-            
+
         .catch(err=>{res.send("there was an unexpected error, please use valid parameters")})
 
     } catch (error) {
         res.send("there was an unexpected error, please use a valid id")
+    }
+
+})
+
+// seeds routes where recCount is the number of required fakes.
+router.post("/todo/seeds/:userId/:recCount", async (req : Request, res: Response)=>{
+    const userId : String= req.params.userId
+    const count = parseInt(req.params.recCount) || 0
+
+    type todos = {
+        userId: String,
+        date: Date,
+        todo: String
+    }
+    
+    const records: {
+        userId: String,
+        date: Date,
+        todo: String
+    }[] =[]
+    
+    for (let index = 0; index < count; index++) {
+        
+        const e : todos = {
+            userId: "6581b0310c119ed079f27fc9",
+            date: faker.date.future(),
+            todo: faker.lorem.paragraph(2)
+        }
+        records.push(e)
+    }
+    
+    try {
+        const userData = await User.findById(userId)
+
+        if(!userData){res.send("user not found")}
+        else{
+            try{
+                await List.create(records)
+                .then(data=>{res.json({result: "todos creared successfully",data})})
+                .catch(err=>{res.send("there was an unexpected error, please use valid parameters")})
+            }
+            catch{
+                res.send("there was an unexpected error, please use valid parameters")
+            }
+        }
+
+    } catch (error) {
+        res.send("there was an unexpected error, please use a valid userId")
     }
 
 })
